d5a5e3d1981489d97a81fd4d9d8b8f2f0ff8cd72
abdo199801 <abdellahbaqba744@gmail.com>
2024-04-09 16:25:44 +0000

    create signup api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
-import {test }from '../controllers/user.controller.js';
+import {test}from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 
-router.get('/test', test 
+router.get('/', test 
 
 );
 
